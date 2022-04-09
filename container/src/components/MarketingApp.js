256e04ee4821ca55928e2876d2fb4cffb8c63ebf
Mori Diakite <moridiakite@hotmail.com>
2022-04-09 23:17:01 +0200

    initial commit

---
+++ container/src/components/MarketingApp.js
@@ -0,0 +1,20 @@
+import React, { useRef, useEffect } from "react";
+import ReactDOM from "react-dom";
+export default () => {
+	const ref = useRef(null);
+
+	useEffect(() => {
+		import("marketing/MarketingApp")
+			.then((module) => {
+				module.mount(ref.current);
+			})
+			.catch((err) => {
+				console.error("Error loading marketing app", err);
+				ReactDOM.findDOMNode(
+					ref.current
+				).innerHTML = `<h2>Error loading marketing app</h2> `;
+			});
+	}, []);
+
+	return <div ref={ref} />;
+};
