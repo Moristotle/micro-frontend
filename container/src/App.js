eb499c1bbf5d3104a2ae4bc3158172c3f76a992b
Mori Diakite <moridiakite@hotmail.com>
2022-04-13 12:11:55 +0200

    randomize production classnames

--- container/src/App.js
+++ container/src/App.js
@@ -1,15 +1,25 @@
+import {
+	createGenerateClassName,
+	StylesProvider,
+} from "@material-ui/core/styles";
 import React from "react";
-import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
 import MarketingApp from "./components/MarketingApp";
 export default () => {
+	const generateClassName = createGenerateClassName({
+		productionPrefix: "co",
+	});
+
 	return (
 		<>
-			<BrowserRouter>
-				<Header />
-				<hr />
-				<MarketingApp />{" "}
-			</BrowserRouter>
+			<StylesProvider generateClassName={generateClassName}>
+				<BrowserRouter>
+					<Header />
+					<hr />
+					<MarketingApp />{" "}
+				</BrowserRouter>
+			</StylesProvider>
 		</>
 	);
 };
--- marketing/src/App.js
+++ marketing/src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
-import { StylesProvider } from "@material-ui/styles";
+import { StylesProvider, createGenerateClassName } from "@material-ui/styles";
 
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
 
-// placeholder!!!!!!!!!!
+// used to create a unique prefix for the generated classnames in production - isolate css for marketing and prevent spillover
+// This is a workaround for the issue: usage of the same component library accross isolated apps and potential clash of css naming
+const generateClassName = createGenerateClassName({
+	productionPrefix: "ma",
+});
+
 export default () => {
 	return (
 		<>
-			<StylesProvider>
+			<StylesProvider generateClassName={generateClassName}>
 				<BrowserRouter>
 					<Switch>
 						<Route exact path="/pricing" component={Pricing} />
