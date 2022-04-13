a02239375cf074e6288fd2ebac561dc243686a87
Mori Diakite <moridiakite@hotmail.com>
2022-04-13 14:50:16 +0200

    configure cross application routing / communication

--- container/src/components/MarketingApp.js
+++ container/src/components/MarketingApp.js
@@ -1,12 +1,23 @@
 import React, { useRef, useEffect } from "react";
 import ReactDOM from "react-dom";
+import { useHistory } from "react-router-dom";
 export default () => {
 	const ref = useRef(null);
+	const history = useHistory();
 
 	useEffect(() => {
 		import("marketing/MarketingApp")
 			.then((module) => {
-				module.mount(ref.current);
+				const { onParentNavigate } = module.mount(ref.current, {
+					onNavigate: ({ pathname: nextPathname }) => {
+						const { pathname } = history.location;
+
+						if (pathname !== nextPathname) {
+							history.push(nextPathname);
+						}
+					},
+				});
+				history.listen(onParentNavigate);
 			})
 			.catch((err) => {
 				console.error("Error loading marketing app", err);
